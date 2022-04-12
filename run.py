7707b9ba5ba8453a9dc6db7a7c61f16ef0e58796
LengGeng <906506482@qq.com>
2022-04-12 21:44:17 +0800

    修复使用 Driver 对 run.py 的影响

--- run.py
+++ run.py
@@ -1,14 +1,13 @@
-from utils.adb import Adb
+from drive import MiniDriver, choose_driver
 from utils.docs import Docs
 from Onmyoji import Onmyoji
 from multiprocessing import Pool
 from multiprocessing import freeze_support
 
 
-def OnmyojiRun(device, fun, *args, **kwargs):
+def OnmyojiRun(serial, fun, *args, **kwargs):
     try:
-        onmyoji = Onmyoji()
-        onmyoji.driver.set_device(device)
+        onmyoji = Onmyoji(MiniDriver(serial))
         onmyoji.init_logger()
         if hasattr(onmyoji, fun):
             function = getattr(onmyoji, fun)
@@ -23,26 +22,24 @@ def get_fun():
     return Docs(Onmyoji).getall()
 
 
-def get_devices():
-    return [device for device in Adb.get_devices_list() if device[1] == "device"]
-
-
 class Run:
     def __init__(self):
-        self.devices = get_devices()
-        if not self.devices:
-            exit('未能获取到设备,请检查设备连接!')
         self.FUN_LIST = get_fun()
 
     def run(self):
         # 循环获取配置
         configs = []
         while True:
-            device = self.connect_device()
-            config = self.get_config(device)
+            # noinspection PyTypeChecker
+            driver = choose_driver(MiniDriver.driver_list())
+            if driver is None:
+                break
+            config = self.get_config(driver.serial)
             configs.append(config)
             if (not self.devices) or input('是否还要添加设备(1 添加/其他 不添加)') != '1':
                 break
+        if not configs:
+            exit('未能获取到设备,请检查设备连接!')
         # 执行
         p = Pool(len(configs))
         for config in configs:
@@ -52,24 +49,6 @@ class Run:
         p.close()
         p.join()
         print('所有子流程都已完成')
-
-    # 连接设备
-    def connect_device(self):
-        print("当前设备列表")
-        print('{:<5}{:<15}{:<}'.format('序号', '设备编号', '类型'))
-        for i, device in enumerate(self.devices):
-            print('{:<7}{:<17}{:<}'.format(i, device[0], device[1]))
-        while True:
-            try:
-                order = int(input('请输入要连接的设备序号：'))
-                device = self.devices[order][0]
-                self.devices.pop(order)
-                print(f"您选择的设备是: {device}")
-                return device
-            except ValueError:
-                print("请输入一个整数")
-            except IndexError:
-                print("输入错误,不存在该设备,请重新选择")
 
     # 获取设备配置
     def get_config(self, device):
