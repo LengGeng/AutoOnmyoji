3b1b8b7ad499138c9cfc6ac03bc56b61c3f5dd8b
LengGeng <906506482@qq.com>
2022-04-15 12:59:54 +0800

    add settings.TEMPLATE_IMAGE_PATH 模板图像路径

--- settings.py
+++ settings.py
@@ -5,4 +5,5 @@ DATE = date.today()
 
 LOG_PATH = os.path.join(os.path.dirname(__file__), "log")  # 日志目录
 LIBS_PATH = os.path.join(os.path.dirname(__file__), "libs")  # 库目录
+TEMPLATE_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "images")  # 模板图片目录
 SCREEN_PATH = os.path.join(os.path.dirname(__file__), "images/screen")  # 截图目录
