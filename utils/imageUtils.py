3ff60da0a068941277031997c633f6aefd756d28
LengGeng <906506482@qq.com>
2022-04-24 14:05:06 +0800

    改进 imageUtils 工具

--- utils/imageUtils.py
+++ utils/imageUtils.py
@@ -1,8 +1,11 @@
-from typing import NewType
+import os
+from typing import NewType, Tuple
 
 import cv2
 import numpy as np
 import base64
+
+from utils.screenUtils import suitable_screensize
 
 CvImage = NewType("CvImage", np.ndarray)  # cv2 ImageObject(np.ndarray)
 
@@ -23,8 +26,11 @@ def save_image(image: CvImage, filepath: str) -> None:
     :param filepath: 文件路径
     :return:
     """
+    name, ext = os.path.splitext(filepath)
+    if not ext:
+        ext = ".jpg"
     with open(filepath, 'wb') as fp:
-        fp.write(cv2bytes(image))
+        fp.write(cv2bytes(image, ext))
 
 
 def bytes2cv(image: bytes) -> CvImage:
@@ -36,16 +42,18 @@ def bytes2cv(image: bytes) -> CvImage:
     return cv2.imdecode(np.array(bytearray(image), dtype='uint8'), cv2.IMREAD_UNCHANGED)  # 从二进制图片数据中读取
 
 
-def cv2bytes(image: CvImage) -> bytes:
+def cv2bytes(image: CvImage, ext: str = ".jpg") -> bytes:
     """
     cv2图片转二进制图片
     :param image: CvImage cv2图片
+    :param ext: 图片格式
     :return: bytesImage 二进制图片数据
     """
-    return np.array(cv2.imencode('.png', image)[1]).tobytes()
+    _, enc = cv2.imencode(ext, image)
+    return np.array(enc).tobytes()
 
 
-def image_to_base64(image_np: bytes) -> str:
+def image_to_base64(image_np: CvImage) -> str:
     """
     将cv2图片转码为base64格式
     image_np: CvImage cv2图片
@@ -71,15 +79,72 @@ def base64_to_image(base64_code: str) -> CvImage:
     return image
 
 
-if __name__ == '__main__':
-    filename = 'test.png'
-    img = cv2.imread(filename, cv2.IMREAD_UNCHANGED)
-    print(type(img))
-    print(img)
-    image_code = image_to_base64(img)
-    # print(image_code)
+def image_size(image: CvImage) -> Tuple[int, int]:
+    """
+    获取图片大小
+    :param image: CvImage cv2图片
+    :return: 图片大小元组
+    """
+    return image.shape[:2][::-1]
 
-    img = base64_to_image(image_code)
-    cv2.imshow('bytes2cv', img)
-    cv2.waitKey(0)
+
+def show(image: CvImage, title: str = 'Image', time: int = 0) -> None:
+    """
+    显示图片
+    :param image: CvImage cv2图片
+    :param title: 窗口标题
+    :param time: 持续时间
+    :return:
+    """
+    cv2.imshow(title, image)
+    cv2.waitKey(time)
     cv2.destroyAllWindows()
+
+
+def show_adapt(image: CvImage, title: str = 'Image', time: int = 0) -> None:
+    """
+    显示图片并自适应屏幕大小
+    :param image: CvImage cv2图片
+    :param title: 窗口标题
+    :param time: 持续时间
+    :return:
+    """
+    # 获取图片大小
+    size = image_size(image)
+    # 获取自适应大小
+    adapt_size = suitable_screensize(size)
+    # float => int
+    adapt_size = tuple(map(int, adapt_size))
+    # 改变图片大小
+    screen_resize = cv2.resize(image, adapt_size)
+    # 显示图片
+    show(screen_resize, title, time)
+
+
+def _test():
+    filename = "../test/image/template.jpg"
+    # 读取图片
+    image = read_image(filename)
+    print(type(image))
+    # 保存图片
+    save_image(image, "../test/image/template.bak.png")
+    # ImageToBytes
+    cv_bytes = cv2bytes(image)
+    # BytesToImage
+    cv_image = bytes2cv(cv_bytes)
+    save_image(cv_image, "../test/image/template.cv_image.png")
+    # ImageToBase64
+    image_base64 = image_to_base64(image)
+    # Base64ToImage
+    base64_image = base64_to_image(image_base64)
+    save_image(base64_image, "../test/image/template.base64_image.png")
+    # ImageSize
+    print(f"{image_size(image)}")
+    # show
+    show(image)
+    # show_adapt
+    show_adapt(image)
+
+
+if __name__ == '__main__':
+    _test()
